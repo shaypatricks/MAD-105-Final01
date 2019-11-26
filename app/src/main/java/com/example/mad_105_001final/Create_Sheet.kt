934ef8e01dd19e4056f2bb6852c89f29af40e5b7
shayes238 <shayes238@students.mchenry.edu>
2019-11-26 11:04:35 -0600

    Changed The Character creation Pages to plain text

--- app/src/main/java/com/example/mad_105_001final/Create_Sheet.kt
+++ app/src/main/java/com/example/mad_105_001final/Create_Sheet.kt
@@ -2,11 +2,34 @@ package com.example.mad_105_001final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Context
 
 class Create_Sheet : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create__sheet)
+
     }
+
+
+
+}
+
+
+enum class Skills{
+    Strength,
+    Dexterity,
+    Constitution,
+    Intelligence,
+    Wisdom,
+    Charisma
+}
+
+
+enum class Class{
+    Fighter,
+    Rogue,
+    Wizard,
+
 }
